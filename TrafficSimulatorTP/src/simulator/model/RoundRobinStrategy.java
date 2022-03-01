7b32c07b938df67d38a04ef4f9881c67b33a5679
danycasperngjay <dany.casperngjay@gmail.com>
2022-03-01 14:38:07 +0100

    no necesary

--- TrafficSimulatorTP/src/simulator/model/RoundRobinStrategy.java
+++ TrafficSimulatorTP/src/simulator/model/RoundRobinStrategy.java
@@ -4,12 +4,6 @@ import java.util.List;
 
 public class RoundRobinStrategy implements LightSwitchingStrategy {
 
-	// Don´t think neccesary
-    //List<Road> roads; //List of incoming roads
-    //List<List<Vehicle>> qs; //the i-th queue (List<Vehicle> is a queue) corresponds to the i-th road in the list "roads"
-    //int currGreen; // the index (in list "roads") of the road with a green light (value -1 if all red)
-    //int lastSwitchingTime; //simulation time at which the light for the road currGreen was switched from red to green (if currGreen -1 then it is the last time all switched to red)
-    //int currTime; // the current simulation time
 	private int timeSlot;
 
     RoundRobinStrategy(int timeSlot){
