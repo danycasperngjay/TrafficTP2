d2c655e1b78189982147658f505126e344ec562c
danycasperngjay <dany.casperngjay@gmail.com>
2022-03-03 20:32:02 +0100

    All tests factories working

---
+++ TrafficSimulatorTP/src/simulator/factories/NewVehicleEventBuilder.java
@@ -0,0 +1,35 @@
+package simulator.factories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import simulator.model.Event;
+import simulator.model.NewVehicleEvent;
+
+public class NewVehicleEventBuilder extends Builder<Event>  {
+	
+	public NewVehicleEventBuilder() {
+		super("new_vehicle");
+	}
+
+	@Override
+	protected Event createTheInstance(JSONObject data) {
+
+		int time = data.getInt("time");
+		String id = data.getString("id");
+		int maxSpeed = data.getInt("maxspeed");
+		int contClass = data.getInt("class");
+		JSONArray iti = new JSONArray();
+		iti = data.getJSONArray("itinerary");
+		List<String> itinerary = new ArrayList<>();  
+        if (iti != null && !iti.isEmpty()) {    
+            for (Object s : iti)    
+               itinerary.add(s.toString());   
+        }
+		return new NewVehicleEvent(time, id, maxSpeed, contClass, itinerary);
+	}
+}
+
