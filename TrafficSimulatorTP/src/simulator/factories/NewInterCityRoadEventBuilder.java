873e6c6c0026e92402f9f9437217e568f67779d4
danycasperngjay <dany.casperngjay@gmail.com>
2022-03-03 20:06:17 +0100

    Create NewInterCityRoadEventBuilder.java

---
+++ TrafficSimulatorTP/src/simulator/factories/NewInterCityRoadEventBuilder.java
@@ -0,0 +1,30 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+
+import simulator.model.Event;
+import simulator.model.NewInterCityRoadEvent;
+import simulator.model.Weather;
+
+public class NewInterCityRoadEventBuilder extends Builder<Event> {
+
+	NewInterCityRoadEventBuilder() {
+		super("new_inter_city_road");
+	}
+
+	@Override
+	protected Event createTheInstance(JSONObject data) {
+		int time = data.getInt("time");
+		String id = data.getString("id");
+		String srcJun = data.getString("src");
+		String destJunc = data.getString("dest");
+		int length = data.getInt("length");
+		int co2Limit = data.getInt("co2limit");
+		int maxSpeed = data.getInt("maxspeed");
+		String s = data.getString("weather");
+		Weather weather = Weather.valueOf(s.toUpperCase());
+		
+		return new NewInterCityRoadEvent(time,id,srcJun,destJunc,length,co2Limit,maxSpeed, weather);
+	}
+
+}
