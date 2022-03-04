ae674a847dd460b2b65f4e692e663a3bded083ea
danycasperngjay <dany.casperngjay@gmail.com>
2022-03-04 13:12:48 +0100

    all factories fixed

--- TrafficSimulatorTP/src/simulator/factories/SetContClassEventBuilder.java
+++ TrafficSimulatorTP/src/simulator/factories/SetContClassEventBuilder.java
@@ -3,11 +3,13 @@ package simulator.factories;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import simulator.misc.Pair;
 import simulator.model.Event;
 import simulator.model.NewSetContClassEvent;
+import simulator.model.Weather;
 
 
 public class SetContClassEventBuilder extends Builder<Event> {
@@ -19,11 +21,17 @@ public class SetContClassEventBuilder extends Builder<Event> {
 	@Override
 	protected Event createTheInstance(JSONObject data) {
 			int time = data.getInt("time");
-			String vehicle = data.getString("vehicle");
-			int cclass = data.getInt("class");
-			Pair<String,Integer> pair = new Pair<String,Integer> (vehicle,cclass);
+			
 			List<Pair<String,Integer>> contClass = new ArrayList<>();
-			contClass.add(pair);
+
+			JSONArray info = data.getJSONArray("info");
+			for (int i = 0; i < info.length();i++)
+			{
+				String vehicle = info.getJSONObject(i).getString("vehicle");
+				int cclass = info.getJSONObject(i).getInt("class");
+				Pair<String,Integer> pair = new Pair<String,Integer> (vehicle,cclass);
+				contClass.add(pair);
+			}
 			return new NewSetContClassEvent(time, contClass);
 	}
 
--- TrafficSimulatorTP/src/simulator/factories/SetWeatherEventBuilder.java
+++ TrafficSimulatorTP/src/simulator/factories/SetWeatherEventBuilder.java
@@ -3,6 +3,7 @@ package simulator.factories;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import simulator.misc.Pair;
@@ -19,12 +20,19 @@ public class SetWeatherEventBuilder extends Builder<Event> {
 	@Override
 	protected Event createTheInstance(JSONObject data) {
 		int time = data.getInt("time");
-		String road = data.getString("road");
-		String s = data.getString("weather");
-		Weather weather = Weather.valueOf(s.toUpperCase());
-		Pair<String,Weather> weatherPair = new Pair<String, Weather> (road,weather);
+		
 		List<Pair<String,Weather>> weatherEvent = new ArrayList<>();
-		weatherEvent.add(weatherPair);
+		
+		JSONArray info = data.getJSONArray("info");
+		for (int i = 0; i < info.length();i++)
+		{
+			String road = info.getJSONObject(i).getString("road");
+			String s = info.getJSONObject(i).getString("weather");
+			Weather weather = Weather.valueOf(s.toUpperCase());
+			Pair<String,Weather> weatherPair = new Pair<String, Weather> (road,weather);
+			weatherEvent.add(weatherPair);
+		}
+		
 		return new SetWeatherEvent(time, weatherEvent);
 	}
 
