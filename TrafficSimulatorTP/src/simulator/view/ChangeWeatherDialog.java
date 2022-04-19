9f3ca15c29d09818a61defc2f15b4434d533c879
lamicebaayno <75587247+lamicebaayno@users.noreply.github.com>
2022-04-19 02:23:51 +0200

    change road weather worked

--- TrafficSimulatorTP/src/simulator/view/ChangeWeatherDialog.java
+++ TrafficSimulatorTP/src/simulator/view/ChangeWeatherDialog.java
@@ -1,14 +1,17 @@
 package simulator.view;
 
 import simulator.control.Controller;
+import simulator.model.Event;
 import simulator.model.Road;
+import simulator.model.RoadMap;
+import simulator.model.TrafficSimObserver;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChangeWeatherDialog extends JDialog {
+public class ChangeWeatherDialog extends JDialog implements TrafficSimObserver {
 
     private static final long serialVersionUID = 1L;
 
@@ -23,11 +26,20 @@ public class ChangeWeatherDialog extends JDialog {
         setLocation(400, 400);
 
         init();
+
     }
 
     public void init(){
-        Frame f = new Frame();
-        JDialog changeWeather = new JDialog(f, "Change Road Weather");
+
+        String[] roadList = {};
+
+        JPanel changeWeather = new JPanel();
+        changeWeather.setLayout(new FlowLayout());
+        this.setContentPane(changeWeather);
+
+        //String title = "Change Road Weather";
+        //Border border = BorderFactory.createTitledBorder(title);
+        //changeWeather.setBorder(border);
 
         //instructions
         JLabel instructions = new JLabel("Schedule an event to change the weather of a road " +
@@ -37,11 +49,12 @@ public class ChangeWeatherDialog extends JDialog {
         //select road
         JLabel road = new JLabel("Road:");
         changeWeather.add(road, BorderLayout.LINE_START);
-        List<String> roadChoices = new ArrayList<>();
-        for(Road r : _ctrl.getSimulator().getRoadMap().getRoads()){
+        ArrayList<String> roadChoices = new ArrayList<>();
+        for (Road r : _ctrl.getSimulator().getRoadMap().getRoads()) {
             roadChoices.add(r.toString());
         }
-        JComboBox comboRoad = new JComboBox((ComboBoxModel) roadChoices);
+        roadList = roadChoices.toArray(new String[0]);
+        JComboBox<String> comboRoad = new JComboBox<String> (roadList);
         comboRoad.setPreferredSize(new Dimension(60, 20));
         changeWeather.add(comboRoad, BorderLayout.LINE_START);
 
@@ -49,7 +62,7 @@ public class ChangeWeatherDialog extends JDialog {
         JLabel weather = new JLabel("Weather:");
         changeWeather.add(weather, BorderLayout.CENTER);
         String[] weatherChoices = {"SUNNY", "WINDY", "CLOUDY", "STORMY", "RAINY"};
-        JComboBox<String> comboWeather= new JComboBox<String>(weatherChoices);
+        JComboBox<String> comboWeather = new JComboBox<String>(weatherChoices);
         comboWeather.setPreferredSize(new Dimension(60, 20));
         changeWeather.add(comboWeather, BorderLayout.CENTER);
 
@@ -60,9 +73,45 @@ public class ChangeWeatherDialog extends JDialog {
         ticksSpinner.setPreferredSize(new Dimension(80, 40));
         changeWeather.add(ticksSpinner, BorderLayout.LINE_END);
 
+
+        JDialog j = new JDialog();
         //cancel and ok button
-        int okCancelOption = JOptionPane.OK_CANCEL_OPTION;
+        j.setDefaultCloseOperation(JOptionPane.OK_CANCEL_OPTION);
+        j.setModal(true);
+        j.add(changeWeather);
+        j.setSize(200, 100);
+        j.setTitle("Change Road Weather");
+        j.setVisible(true);
+    }
+
+
+    @Override
+    public void onAdvanceEnd(RoadMap roadMap, List<Event> events, int time) {
 
     }
 
+    @Override
+    public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
+
+    }
+
+    @Override
+    public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
+
+    }
+
+    @Override
+    public void onReset(RoadMap map, List<Event> events, int time) {
+
+    }
+
+    @Override
+    public void onRegister(RoadMap map, List<Event> events, int time) {
+
+    }
+
+    @Override
+    public void onError(String err) {
+
+    }
 }
